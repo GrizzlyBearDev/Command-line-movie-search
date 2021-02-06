de4e34b4c2865814182ac54e4fdcfd179eefedc2
Bear <Bear@grizzlybear.dev>
2021-02-06 21:42:06 +0200

    Commenting and cleanup

--- lib/movie.dart
+++ lib/movie.dart
@@ -5,40 +5,38 @@ import 'package:http/http.dart' as Http;
 import 'package:dotenv/dotenv.dart' show load, env;
 void main(List<String> arguments) async {
 
+  // Loading the .env
   load();
 
   stdout.writeln('Enter the name of a movie or show:');
   stdout.writeln('----------------------------------');
 
+  // Getting search input from user
   String input = stdin.readLineSync();
 
-  Map<String, String> headers = HashMap();
+  //API URL
+  String url = 'http://www.omdbapi.com/?t=${Uri.encodeFull(input)}&apikey=${env['apikey']}';
 
-  String url = 'http://www.omdbapi.com/?t=${Uri.encodeFull(input)}&apikey=${env['fmovies']}';
+  //Get request using the URL
+  Http.Response response = await Http.get(url);
 
-  Http.Response response = await Http.get(
-        url,
-        headers: headers,
-    );
-
+  //Parses the response body and assigns it to movie Map
   Map movie = jsonDecode(response.body);
 
-  // print(movie);
-
   print('----------------------------------');
-  print('TITLE: ' + movie['Title']);
-  print('YEAR: ' + movie['Year']);
-  print('GENRE: ' + movie['Genre']);
-  print('PLOT: ' + movie['Plot']);
-  print('TYPE: ' + movie['Type']);
-  print('ACTORS: ' + movie['Actors']);
-  print('IMDB RATING: ' + movie['imdbRating']);
+  print('TITLE: ${movie['Title']}');
+  print('YEAR: ${movie['Year']}');
+  print('GENRE: ${movie['Genre']}');
+  print('PLOT: ${movie['Plot']}');
+  print('TYPE: ${movie['Type']}');
+  print('ACTORS: ${movie['Actors']}');
+  print('IMDB RATING: ${movie['imdbRating']}');
 
   movie['Ratings'].forEach((r) => {
     print(r['Source'].toUpperCase() + ': ' + r['Value'])
   });
 
-  print('POSTER: ' + movie['Poster']);
-  print('LINK: https://www.imdb.com/title/' + movie['imdbID']);
+  print('POSTER: ${movie['Poster']}');
+  print('LINK: https://www.imdb.com/title/${movie['imdbID']}');
 
 }
